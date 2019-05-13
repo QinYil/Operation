d939ad156ffeda66fc5188ee456cb77840cddcf4
QinYil <619531306@qq.com>
2019-05-13 15:01:30 +0800

    answer for operation

---
+++ untitled/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Operation operation = new Operation();
+        System.out.println("一共:"+operation.leastOpsExpressTarget(3,365));
+    }
+}
